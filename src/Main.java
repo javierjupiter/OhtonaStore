506b9eefbc781fdf10bc1e0e74c51aa7fd5c3a96
javierjupiter <javierjupiters@gmail.com>
2020-07-12 06:03:00 -0500

    And in the end, the love you take, is equal to the love, you make

--- src/Main.java
+++ src/Main.java
@@ -11,7 +11,7 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("Catalogo_UI.fxml"));
         primaryStage.setTitle("Ohtona Store");
-        primaryStage.setResizable(true);
+        primaryStage.setResizable(false);
         Image icon = new Image(getClass().getResourceAsStream("/images/ohtona_mini.png"));
         primaryStage.getIcons().add(icon);
         primaryStage.setScene(new Scene(root, 1000, 600));
